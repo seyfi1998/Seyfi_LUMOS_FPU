347b5b7ed3d341f3c47b6313a77f30a4c4dfe2db
seyfi1998 <>
2024-06-25 13:36:40 +0330

    Adding 32-bit Multiplier block
    Description:
    Adding 32-bit Multiplier block to Fixed_Point_Unit.v

--- Fixed_Point_Unit.v
+++ Fixed_Point_Unit.v
@@ -69,6 +69,85 @@ module Fixed_Point_Unit
         /*
          *  Describe Your 32-bit Multiplier Circuit Here.
          */
+    //Three bit register to store 32-bit multiplication state
+    reg [2:0] state;
+
+    //Define parameters for each state
+    localparam INITIALSTATE = 3'b000;
+    localparam FIRSTMUL = 3'b001;
+    localparam SECONDMUL = 3'b010;
+    localparam THIRDMUL = 3'b011;
+    localparam FOURTHMUL = 3'b100;
+    localparam ADD = 3'b101;
+
+    always @(posedge clk) begin
+        if (reset) begin
+            state <= INITIALSTATE;
+            product <= 0;
+            product_ready <= 0;
+        end else begin
+            case (state)
+                //Initial state to initialize registers
+                INITIALSTATE: begin
+                    if (operation == `FPU_MUL) begin
+                        //In FIRSTMUL stage inpust will be first 16-bit of each input
+                        multiplierCircuitInput1 <= operand_1[15 : 0];
+                        multiplierCircuitInput2 <= operand_2[15 : 0];
+                        //Partial products should be 0 at start
+                        partialProduct1 <= 0;
+                        partialProduct2 <= 0;
+                        partialProduct3 <= 0;
+                        partialProduct4 <= 0;
+                        // Change state to FIRSTMUL at the next cycle
+                        state <= FIRSTMUL;
+                    end
+                end
+                FIRSTMUL: begin
+                    // Store partial product result
+                    partialProduct1 <= multiplierCircuitResult;
+                    // Update multiplier inputs to calculate partial proct 2
+                    multiplierCircuitInput1 <= operand_1[31 : 16];
+                    multiplierCircuitInput2 <= operand_2[15 : 0];
+                    // Change state to be SECONDMUL at next cycle
+                    state <= SECONDMUL;
+                end
+                SECONDMUL: begin
+                    // Store partial product result with 16-bit shift to the left
+                    partialProduct2 <= multiplierCircuitResult << 16;
+                    // Update multiplier inputs to calculate partial proct 3
+                    multiplierCircuitInput1 <= operand_1[15 : 0];
+                    multiplierCircuitInput2 <= operand_2[31 : 16];
+                    // Change state to be THIRDMUL at next cycle
+                    state <= THIRDMUL;
+                end
+                THIRDMUL: begin
+                    // Store partial product with 16-bit shift to the left
+                    partialProduct3 <= multiplierCircuitResult << 16;
+                    // Update multiplier inputs to calculate partial proct 4
+                    multiplierCircuitInput1 <= operand_1[31 : 16];
+                    multiplierCircuitInput2 <= operand_2[31 : 16];
+                    // Change state to be FOURTHMUL at next cycle
+                    state <= FOURTHMUL;
+                end
+                FOURTHMUL: begin
+                    // Store partial product with 32-bit shift to the left
+                    partialProduct4 <= multiplierCircuitResult << 32;
+                    //  Change state to be ADD at next cycle
+                    state <= ADD;
+                end
+                ADD: begin
+                    // Store partial product
+                    product <= partialProduct1 + partialProduct2 + partialProduct3 + partialProduct4;
+                    // Move to next state
+                    state <= INITIALSTATE;
+                    product_ready <= 1; 
+                end
+                default: begin
+                    state <= INITIALSTATE;
+                end
+            endcase
+        end
+    end
          
 endmodule
 
